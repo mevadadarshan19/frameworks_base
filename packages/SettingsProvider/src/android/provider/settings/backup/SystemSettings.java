a40b52f3fdf9db36e3976c20f4f072bee590bfcc
Ido Ben-Hur <idoybh2@gmail.com>
2019-11-28 15:26:44 +0200

    SystemUI: Allow customizing footer text [1/2]
    [jhonboy121]: simplified conditionals
    
    Change-Id: I87d5197f37ae86808bafa71d62dae8cf5ca4f8f3
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -100,5 +100,6 @@ public class SystemSettings {
         Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD,
         Settings.System.USE_OLD_MOBILETYPE,
         Settings.System.QS_FOOTER_TEXT_SHOW,
+        Settings.System.QS_FOOTER_TEXT_STRING,
     };
 }
