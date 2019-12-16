0f509fec983d2c99643cab4b5592268731bf2aa3
PMS22 <pratham220399@gmail.com>
2019-12-16 00:16:34 -0700

    Add Flashlight blink on incoming calls [1/3]
    - There are tons of apps out there on playstore which replicate the same
      iOS behaviour. Let's just add it in system keeping it disabled by default.
    
    Change-Id: I9d873f4b145416fc13220d3b4da98da1ce638e9e

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -92,5 +92,6 @@ public class SystemSettings {
         Settings.System.QS_SHOW_BATTERY_ESTIMATE,
         Settings.System.STATUS_BAR_BATTERY_STYLE,
         Settings.System.SHOW_BATTERY_PERCENT_INSIDE,
+        Settings.System.FLASHLIGHT_ON_CALL,
     };
 }
