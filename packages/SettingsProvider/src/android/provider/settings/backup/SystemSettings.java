681d56a68d136317ccdd25df35a5aa88a068fb2d
Ido Ben-Hur <idoybh2@gmail.com>
2020-06-13 03:27:56 +0300

    base: Add DND & Rate settings to flash on call [1/3]
    Change-Id: I23f19787bca6fd2918c3f5be50b012cf2cfdbc89

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -93,5 +93,7 @@ public class SystemSettings {
         Settings.System.STATUS_BAR_BATTERY_STYLE,
         Settings.System.SHOW_BATTERY_PERCENT_INSIDE,
         Settings.System.FLASHLIGHT_ON_CALL,
+        Settings.System.FLASHLIGHT_ON_CALL_IGNORE_DND,
+        Settings.System.FLASHLIGHT_ON_CALL_RATE,
     };
 }
