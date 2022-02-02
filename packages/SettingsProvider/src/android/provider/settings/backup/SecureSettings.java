9d8d9a5c6330a6345712b0f4bbbfee90c431a574
Pranav <npv12@iitbbs.ac.in>
2022-02-03 01:54:17 +0530

    SystemUI: Improve Secure QS tile behavior toggle [1/2]
    * Instead of checking settings in handleClick, added a settings observer and updates tiles on change
    * Moved the setting key to Settings.Secure class since it's more apt
    * Also guarded so debug logs
    
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -222,5 +222,6 @@ public class SecureSettings {
         Settings.Secure.IGNORE_AUTH_CONFIRMATION,
         Settings.Secure.GESTURE_NAVBAR_LENGTH_MODE,
         Settings.Secure.POWER_MENU_HIDE_ON_SECURE,
+        Settings.Secure.DISABLE_SECURE_TILES_ON_LOCKSCREEN,
     };
 }
