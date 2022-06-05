77433bbbe25c43f3448f75e4af0b3e1ba62a147a
minarypenguin <alexfinhart@gmail.com>
2022-06-05 13:55:40 +0800

    base: Clean up Custom logo customizations [1/2]

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -112,9 +112,7 @@ public class SystemSettings {
         Settings.System.NOTIFICATION_PULSE_DURATION,
         Settings.System.ENABLE_FLOATING_ROTATION_BUTTON,
         Settings.System.STATUS_BAR_LOGO,
-        Settings.System.STATUS_BAR_LOGO_COLOR,
         Settings.System.STATUS_BAR_LOGO_POSITION,
         Settings.System.STATUS_BAR_LOGO_STYLE,
-        Settings.System.STATUS_BAR_LOGO_COLOR_ACCENT,
     };
 }
