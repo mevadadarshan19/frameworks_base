bf8ff5364acfc2c0db0e9997a8e486f9f5fc3309
minaripenguin <alexfinhart@gmail.com>
2022-07-26 19:15:04 +0800

    SystemUI: Secondary color for lockscreen clock toggle [1/2]
    Signed-off-by: minaripenguin <alexfinhart@gmail.com>
    Signed-off-by: str143 <alexfinhart@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6242,6 +6242,11 @@ public final class Settings {
         public static final String STATUSBAR_CLOCK_CHIP ="statusbar_clock_chip";
 
         /**
+         * @hide
+         */
+        public static final String SECONDARY_COLOR_CLOCK = "use_secondary_color_clock";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
