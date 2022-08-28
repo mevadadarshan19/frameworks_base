b56809882014ddeab2fa0efb5d021d66ee1e011a
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-08-28 15:29:46 +0530

    Revert "Sysui changes to support vibrate icon setting"
    We already have statusbar tuner for it.
    
    This reverts commit 5b5a16e90073f7d4e75dfb7743838c6f26f17d42.
    
    Change-Id: Ie9460a7f00d45b73737e1a2a8aafef9aaac0f55c

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
@@ -49,13 +49,10 @@ import android.view.ViewGroup;
 import android.view.ViewStub;
 import android.widget.LinearLayout;
 
-import androidx.annotation.VisibleForTesting;
-
 import com.android.systemui.Dependency;
 import com.android.systemui.Dumpable;
 import com.android.systemui.R;
 import com.android.systemui.animation.Interpolators;
-import com.android.systemui.dagger.qualifiers.Main;
 import com.android.systemui.dump.DumpManager;
 import com.android.systemui.flags.FeatureFlags;
 import com.android.systemui.plugins.statusbar.StatusBarStateController;
@@ -89,7 +86,6 @@ import com.android.systemui.statusbar.policy.KeyguardStateController;
 import com.android.systemui.util.CarrierConfigTracker;
 import com.android.systemui.util.CarrierConfigTracker.CarrierConfigChangedListener;
 import com.android.systemui.util.CarrierConfigTracker.DefaultDataSubscriptionChangedListener;
-import com.android.systemui.util.settings.SecureSettings;
 import com.android.systemui.tuner.TunerService;
 
 import org.jetbrains.annotations.NotNull;
@@ -100,7 +96,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.Executor;
 
 /**
  * Contains the collapsed status bar and handles hiding/showing based on disable flags
@@ -148,8 +143,6 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
     private final CarrierConfigTracker mCarrierConfigTracker;
     private final StatusBarHideIconsForBouncerManager mStatusBarHideIconsForBouncerManager;
     private final StatusBarIconController.DarkIconManager.Factory mDarkIconManagerFactory;
-    private final SecureSettings mSecureSettings;
-    private final Executor mMainExecutor;
     private final DumpManager mDumpManager;
     private ClockController mClockController;
     private boolean mIsClockBlacklisted;
@@ -238,8 +231,6 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
             CarrierConfigTracker carrierConfigTracker,
             CollapsedStatusBarFragmentLogger collapsedStatusBarFragmentLogger,
             OperatorNameViewController.Factory operatorNameViewControllerFactory,
-            SecureSettings secureSettings,
-            @Main Executor mainExecutor,
             DumpManager dumpManager
     ) {
         mStatusBarFragmentComponentFactory = statusBarFragmentComponentFactory;
@@ -260,8 +251,6 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         mCarrierConfigTracker = carrierConfigTracker;
         mCollapsedStatusBarFragmentLogger = collapsedStatusBarFragmentLogger;
         mOperatorNameViewControllerFactory = operatorNameViewControllerFactory;
-        mSecureSettings = secureSettings;
-        mMainExecutor = mainExecutor;
         mDumpManager = dumpManager;
     }
 
@@ -297,7 +286,9 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         mContentResolver = getContext().getContentResolver();
         mSettingsObserver = new SettingsObserver(mHandler);
         mDarkIconManager.setShouldLog(true);
-        updateBlockedIcons();
+        mBlockedIcons = Arrays.asList(getResources().getStringArray(
+                R.array.config_collapsed_statusbar_icon_blocklist));
+        mDarkIconManager.setBlockList(mBlockedIcons);
         mStatusBarIconController.addIconGroup(mDarkIconManager);
         mEndSideContent = mStatusBar.findViewById(R.id.status_bar_end_side_content);
         mClockController = mStatusBar.getClockController();
@@ -325,39 +316,6 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         updateSettings(false);
     }
 
-    @VisibleForTesting
-    void updateBlockedIcons() {
-        mBlockedIcons.clear();
-
-        // Reload the blocklist from res
-        List<String> blockList = Arrays.asList(getResources().getStringArray(
-                R.array.config_collapsed_statusbar_icon_blocklist));
-        String vibrateIconSlot = getString(com.android.internal.R.string.status_bar_volume);
-        boolean showVibrateIcon =
-                mSecureSettings.getIntForUser(
-                        Settings.Secure.STATUS_BAR_SHOW_VIBRATE_ICON,
-                        0,
-                        UserHandle.USER_CURRENT) == 0;
-
-        // Filter out vibrate icon from the blocklist if the setting is on
-        for (int i = 0; i < blockList.size(); i++) {
-            if (blockList.get(i).equals(vibrateIconSlot)) {
-                if (showVibrateIcon) {
-                    mBlockedIcons.add(blockList.get(i));
-                }
-            } else {
-                mBlockedIcons.add(blockList.get(i));
-            }
-        }
-
-        mMainExecutor.execute(() -> mDarkIconManager.setBlockList(mBlockedIcons));
-    }
-
-    @VisibleForTesting
-    List<String> getBlockedIcons() {
-        return mBlockedIcons;
-    }
-
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
@@ -373,12 +331,6 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         mStatusBarStateController.addCallback(this);
         initOngoingCallChip();
         mAnimationScheduler.addCallback(this);
-
-        mSecureSettings.registerContentObserverForUser(
-                Settings.Secure.getUriFor(Settings.Secure.STATUS_BAR_SHOW_VIBRATE_ICON),
-                false,
-                mVolumeSettingObserver,
-                UserHandle.USER_ALL);
     }
 
     @Override
@@ -388,7 +340,6 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         mStatusBarStateController.removeCallback(this);
         mOngoingCallController.removeCallback(mOngoingCallListener);
         mAnimationScheduler.removeCallback(this);
-        mSecureSettings.unregisterContentObserver(mVolumeSettingObserver);
     }
 
     @Override
@@ -774,13 +725,6 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
 
         mLocationPublisher.updateStatusBarMargin(leftMargin, rightMargin);
     }
-
-    private final ContentObserver mVolumeSettingObserver = new ContentObserver(null) {
-        @Override
-        public void onChange(boolean selfChange) {
-            updateBlockedIcons();
-        }
-    };
 
     // Listen for view end changes of PhoneStatusBarView and publish that to the privacy dot
     private View.OnLayoutChangeListener mStatusBarLayoutListener =
