6b9dc215ce2259167f0885c636683bdcff1c4765
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-09-18 17:20:32 +0530

    SystemUI: Improve statusbar mobile type icons padding
    The new "Silk design" mobile icons introduced in [1]
    have 2dp of spacing inside it on each sides. This is
    quite unnecessary and looks ugly as the mobile layout
    already provides 2.5dp of padding.
    
    For the sake of consistency, remove this spacing using
    insets and rely only on padding. Add space only for
    the second SIM's mobile data type icon.
    
    While we're at it, always use 4G icon instead of LTE
    to reduce space usage.
    
    [1]: 084c13c8216f6a899cd3eda04fc1d7acff3d1248 ("Update RAT icons to match Silk design")
    
    Change-Id: I4d703e37983ee45da2219936156028d5d40f31a0

--- packages/SystemUI/src/com/android/systemui/statusbar/StatusBarMobileView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/StatusBarMobileView.java
@@ -61,7 +61,7 @@ public class StatusBarMobileView extends FrameLayout implements DarkReceiver,
     private ImageView mOut;
     private ImageView mMobile, mMobileType, mMobileRoaming;
     private View mMobileSignalType;
-    private View mMobileRoamingSpace;
+    private View mMobileTypeSpace, mMobileRoamingSpace;
     @StatusBarIconView.VisibleState
     private int mVisibleState = STATE_HIDDEN;
     private DualToneHandler mDualToneHandler;
@@ -131,6 +131,7 @@ public class StatusBarMobileView extends FrameLayout implements DarkReceiver,
         mMobileType = findViewById(R.id.mobile_type);
         mMobileRoaming = findViewById(R.id.mobile_roaming);
         mMobileRoamingSpace = findViewById(R.id.mobile_roaming_space);
+        mMobileTypeSpace = findViewById(R.id.mobile_type_space);
         mIn = findViewById(R.id.mobile_in);
         mOut = findViewById(R.id.mobile_out);
         mInoutContainer = findViewById(R.id.inout_container);
@@ -203,6 +204,7 @@ public class StatusBarMobileView extends FrameLayout implements DarkReceiver,
             mMobileTypeSmall.setVisibility(View.GONE);
             setMobileSignalWidth(true);
         }
+        mMobileTypeSpace.setVisibility(mState.typeSpacerVisible ? View.VISIBLE : View.GONE);
         mMobile.setVisibility(mState.showTriangle ? View.VISIBLE : View.GONE);
         mMobileRoaming.setVisibility(mState.roaming ? View.VISIBLE : View.GONE);
         mMobileRoamingSpace.setVisibility(mState.roaming || showRoamingSpace ? View.VISIBLE : View.GONE);
@@ -258,7 +260,7 @@ public class StatusBarMobileView extends FrameLayout implements DarkReceiver,
                 hideIndicators();
             }
         }
-
+        mMobileTypeSpace.setVisibility(state.typeSpacerVisible ? View.VISIBLE : View.GONE);
         mMobile.setVisibility(state.showTriangle ? View.VISIBLE : View.GONE);
         if (state.roaming) {
             mMobileTypeSmall.setVisibility(View.GONE);
