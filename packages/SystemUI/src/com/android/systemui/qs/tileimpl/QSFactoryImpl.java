856af6bfafdf9070deca8744f6880bf0c8f21be0
Anay Wadhera <anay1018@gmail.com>
2022-10-04 21:55:55 +0900

    SystemUI: introduce Flashlight Strength Tile
    This tile allows the user to control flashlight brightness on supported devices while acting like a normal flashlight tile on unsupported devices
    
    Change-Id: Id346a1c170170c21f8f619e265e30a36f7adeaf2
    [nurkeinneid] move to aosp sysui
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -27,6 +27,11 @@ import com.android.systemui.plugins.qs.QSTile;
 import com.android.systemui.plugins.qs.QSTileView;
 import com.android.systemui.qs.QSHost;
 import com.android.systemui.qs.external.CustomTile;
+import com.android.systemui.qs.tileimpl.QSFactoryImpl;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
+import com.android.systemui.qs.tileimpl.QSTileViewImpl;
+import com.android.systemui.qs.tileimpl.SliderQSTileViewImpl;
+import com.android.systemui.qs.tileimpl.TouchableQSTile;
 import com.android.systemui.qs.tiles.DerpSpaceTile;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
 import com.android.systemui.qs.tiles.AutoBrightnessTile;
@@ -46,7 +51,7 @@ import com.android.systemui.qs.tiles.CompassTile;
 import com.android.systemui.qs.tiles.DataSaverTile;
 import com.android.systemui.qs.tiles.DeviceControlsTile;
 import com.android.systemui.qs.tiles.DndTile;
-import com.android.systemui.qs.tiles.FlashlightTile;
+import com.android.systemui.qs.tiles.FlashlightStrengthTile;
 import com.android.systemui.qs.tiles.GloveModeTile;
 import com.android.systemui.qs.tiles.HeadsUpTile;
 import com.android.systemui.qs.tiles.HotspotTile;
@@ -80,6 +85,8 @@ import com.android.systemui.qs.tiles.WorkModeTile;
 import com.android.systemui.qs.tiles.DataSwitchTile;
 import com.android.systemui.util.leak.GarbageMonitor;
 
+import java.util.Arrays;
+
 import javax.inject.Inject;
 import javax.inject.Provider;
 
@@ -90,6 +97,7 @@ public class QSFactoryImpl implements QSFactory {
 
     private static final String TAG = "QSFactory";
 
+    private static final String[] SLIDER_TILES = { "flashlight" };
     private final Provider<WifiTile> mWifiTileProvider;
     private final Provider<InternetTile> mInternetTileProvider;
     private final Provider<BluetoothTile> mBluetoothTileProvider;
@@ -100,7 +108,7 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<AirplaneModeTile> mAirplaneModeTileProvider;
     private final Provider<WorkModeTile> mWorkModeTileProvider;
     private final Provider<RotationLockTile> mRotationLockTileProvider;
-    private final Provider<FlashlightTile> mFlashlightTileProvider;
+    private final Provider<FlashlightStrengthTile> mFlashlightTileProvider;
     private final Provider<LocationTile> mLocationTileProvider;
     private final Provider<CastTile> mCastTileProvider;
     private final Provider<HotspotTile> mHotspotTileProvider;
@@ -159,7 +167,7 @@ public class QSFactoryImpl implements QSFactory {
             Provider<AirplaneModeTile> airplaneModeTileProvider,
             Provider<WorkModeTile> workModeTileProvider,
             Provider<RotationLockTile> rotationLockTileProvider,
-            Provider<FlashlightTile> flashlightTileProvider,
+            Provider<FlashlightStrengthTile> flashlightTileProvider,
             Provider<LocationTile> locationTileProvider,
             Provider<CastTile> castTileProvider,
             Provider<HotspotTile> hotspotTileProvider,
@@ -215,7 +223,7 @@ public class QSFactoryImpl implements QSFactory {
         mAirplaneModeTileProvider = airplaneModeTileProvider;
         mWorkModeTileProvider = workModeTileProvider;
         mRotationLockTileProvider = rotationLockTileProvider;
-        mFlashlightTileProvider = flashlightTileProvider;
+        mFlashlightTileProvider = flashlightTileProvider::get;
         mLocationTileProvider = locationTileProvider;
         mCastTileProvider = castTileProvider;
         mHotspotTileProvider = hotspotTileProvider;
@@ -400,6 +408,10 @@ public class QSFactoryImpl implements QSFactory {
     @Override
     public QSTileView createTileView(Context context, QSTile tile, boolean collapsedView) {
         QSIconView icon = tile.createTileView(context);
+        if (Arrays.asList(SLIDER_TILES).contains(tile.getTileSpec())) {
+            TouchableQSTile touchableTile = (TouchableQSTile) tile;
+            return new SliderQSTileViewImpl(context, icon, collapsedView, touchableTile.getTouchListener(), touchableTile.getSettingsSystemKey());
+        }
         return new QSTileViewImpl(context, icon, collapsedView);
     }
 }
