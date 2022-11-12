c500eca69b49656335b6d1ab6500e97a340aac20
jhonboy121 <alfredmathew05@gmail.com>
2022-11-12 21:13:57 +0100

    SystemUI: VolumeDialogControllerImpl: make sure to load linked notification state as well
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogControllerImpl.java
@@ -506,6 +506,7 @@ public class VolumeDialogControllerImpl implements VolumeDialogController, Dumpa
         updateRingerModeExternalW(mRingerModeObservers.mRingerMode.getValue());
         updateZenModeW();
         updateZenConfig();
+        updateLinkNotificationConfigW();
         updateEffectsSuppressorW(mNoMan.getEffectsSuppressor());
         mCallbacks.onStateChanged(mState);
     }
@@ -1058,7 +1059,7 @@ public class VolumeDialogControllerImpl implements VolumeDialogController, Dumpa
                 changed |= updateZenConfig();
             }
             if (VOLUME_LINK_NOTIFICATION_URI.equals(uri)) {
-                changed = updateLinkNotificationConfigW();
+                changed |= updateLinkNotificationConfigW();
             }
 
             if (changed) {
