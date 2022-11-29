f2cff5f30ccba349dc5078d9a30740f49bc49fa6
Blaster4385 <venkateshchaturvedi12@gmail.com>
2022-11-29 22:03:03 +0530

    base: Add optional haptic feedback to brightness slider [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6693,6 +6693,12 @@ public final class Settings {
          public static final String UDFPS_ICON_ACCENT = "udfps_icon_accent";
 
         /**
+         * Haptic feedback on brightness slider
+         * @hide
+         */ 
+        public static final String BRIGHTNESS_SLIDER_HAPTIC_FEEDBACK = "brightness_slider_haptic_feedback";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
