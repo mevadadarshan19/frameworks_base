b6ac11399ce98c9e33d3dc18bd3504cd1e6ecdcd
Idc <59904122+IDontCare-05@users.noreply.github.com>
2023-04-06 16:16:52 +0800

    SystemUI: QS Header Image [1/2]
    * reverse engineered from ancient os with [1] as reference for broken codes from jadx
    * Optimized qs banner resources for systemui file size
    * all credits goes to ancientOS
    
    [1]: https://github.com/Hikari-no-Tenshi/crdroid_android_frameworks_base/commit/354d981d3f79783e284c8aced755e90496324982
    
    mnri: rewrite and refactored for risingOS
    Darkstar085: Banners update
    Co-authored-by: Idc <59904122+IDontCare-05@users.noreply.github.com>
    Change-Id: I23a0e870f092ca088bfaae018db50425d7d6bf37
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6714,6 +6714,12 @@ public final class Settings {
         public static final String PREVENT_POINTER_ACCELERATION = "prevent_pointer_acceleration";
 
         /**
+         * QS Header
+         * @hide
+         */
+        public static final String QS_HEADER_IMAGE = "qs_header_image";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
