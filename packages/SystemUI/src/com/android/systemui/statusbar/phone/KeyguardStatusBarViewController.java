bf71b4f5e23fbdcc74b5438ed072a39863acbb57
NurKeinNeid <mralexman3000@gmail.com>
2023-04-15 10:39:55 +0200

    Revert "SystemUI: Ensure keyguard statusbar is gone when not showing"
    This reverts commit 5fe367fea0775586f26978941c7d672812bc0da0.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarViewController.java
@@ -443,7 +443,6 @@ public class KeyguardStatusBarViewController extends ViewController<KeyguardStat
      */
     public void updateViewState() {
         if (!isKeyguardShowing()) {
-            mView.setVisibility(View.GONE);
             return;
         }
 
